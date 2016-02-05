39941f6aa99bc50579caa4576e96749aeefbef75
William Adjei <will@pixellabs.com>
2016-02-05 11:49:35 +0000

    Full test coverage of SmokeTestController template

--- tests/phpspec/SainsburysHaraSpec/Sainsburys/Hara/Xxxxxx/Controller/SmokeTestControllerSpec.php
+++ tests/phpspec/SainsburysHaraSpec/Sainsburys/Hara/Xxxxxx/Controller/SmokeTestControllerSpec.php
@@ -3,11 +3,32 @@ namespace SainsburysHaraSpec\Sainsburys\Hara\Xxxxxx\Controller;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Zend\Diactoros\ServerRequest;
+use Slim\Http\Response;
+use Teapot\StatusCode\Http;
 
 class SmokeTestControllerSpec extends ObjectBehavior
 {
     function it_is_initialisable()
     {
         $this->shouldHaveType('Sainsburys\Hara\Xxxxxx\Controller\SmokeTestController');
+    }
+
+    function it_passes_when_called()
+    {
+        $requestPayload = fopen('php://memory','r+');
+        fwrite($requestPayload, json_encode([]));
+        rewind($requestPayload);
+
+        $request  = new ServerRequest([], [], '/api.com/smoke-test', 'POST', $requestPayload);
+        $response = new Response();
+
+        $this->smokeTestAction($request, $response);
+        $payload = json_decode($response->getBody());
+
+        \PHPUnit_Framework_Assert::assertEquals(Http::OK, $response->getStatusCode());
+        \PHPUnit_Framework_Assert::assertObjectHasAttribute('result', $payload);
+        \PHPUnit_Framework_Assert::assertEquals('PASS', $payload->result);
+
     }
 }
